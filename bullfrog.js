e74630ffc441e4bd7fed0dff5eaa1aedc986bc4e
Bullfrog <justin.michael.gordon@gmail.com>
2023-05-15 13:15:17 -0700

    fixing cs on mode select

--- bullfrog.js
+++ bullfrog.js
@@ -2,21 +2,6 @@ function changeBottomImage(src) {
     document.getElementById("bottom-img").src = src;
   }
 
-  const darkToggle = document.getElementById('toggle-darktheme');
-  darkToggle.addEventListener('click', function() {
-    const themeStylesheet = document.querySelector('#style-mode');
-    if (themeStylesheet.getAttribute('href') === 'bullfrog.css' || themeStylesheet.getAttribute('href') === 'partymode.css') {
-      document.getElementById("bottom-img").src = 'imgs/navmenu0.jpg';
-      themeStylesheet.setAttribute('href', 'darkmode.css');
-      localStorage.setItem('stylemode', 'dark')
-    } else {
-      themeStylesheet.setAttribute('href', 'bullfrog.css')
-      localStorage.setItem('stylemode', 'light')
-      document.getElementById("bottom-img").src = 'imgs/navmenu0.jpg';
-
-    }
-  });
-
   if (localStorage.getItem('stylemode') === 'dark') {
     const themeStylesheet = document.querySelector('#style-mode');
     themeStylesheet.setAttribute('href', 'darkmode.css');
@@ -29,17 +14,39 @@ function changeBottomImage(src) {
     const themeStylesheet = document.querySelector('#style-mode');
     themeStylesheet.setAttribute('href', 'partymode.css');
   }
+
+  const darkToggle = document.getElementById('toggle-darktheme');
+  darkToggle.addEventListener('click', function() {
+    const themeStylesheet = document.querySelector('#style-mode');
+    if (themeStylesheet.getAttribute('href') !== 'darkmode.css') {
+      if (document.getElementById("bottom-img") ){
+        document.getElementById("bottom-img").src = 'imgs/navmenu0.jpg'};
+      themeStylesheet.setAttribute('href', 'darkmode.css');
+      localStorage.setItem('stylemode', 'dark')
+    } else {
+      themeStylesheet.setAttribute('href', 'bullfrog.css')
+      localStorage.setItem('stylemode', 'light')
+      if (document.getElementById("bottom-img") ){
+        document.getElementById("bottom-img").src = 'imgs/navmenu0.jpg'};;
+
+    }
+  });
+
   const partyTheme = document.getElementById('toggle-partytheme');
   partyTheme.addEventListener('click', function() {
     const themeStylesheet = document.querySelector('#style-mode');
-    if (themeStylesheet.getAttribute('href') === 'bullfrog.css' || themeStylesheet.getAttribute('href') === 'darkmode.css'){
+    if (themeStylesheet.getAttribute('href') !== 'partymode.css'){
       themeStylesheet.setAttribute('href', 'partymode.css');
-      document.getElementById("bottom-img").src = 'imgs/partymenu.jpg';
+      if (document.getElementById("bottom-img") ){
+        document.getElementById("bottom-img").src = 'imgs/partymenu.jpg'};;
       localStorage.setItem('stylemode', 'party')
     } else {
-      document.getElementById("bottom-img").src = 'imgs/navmenu0.jpg'
+      if (document.getElementById("bottom-img") ){
+        document.getElementById("bottom-img").src = 'imgs/navmenu0.jpg'};
       themeStylesheet.setAttribute('href', 'bullfrog.css')
       localStorage.setItem('stylemode', 'light')
     }
   });
+
+
 
